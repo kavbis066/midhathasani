39a5ed0f305a0e5f87adbd0641352e85aff9c9d1
midhathasani <midhat.human@gmail.com>
2026-09-14 16:25:51 +0530

    Update Header.svelte

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -68,7 +68,7 @@
                 </a>
             {/each}
         </div>
-        <a href="/Resume.pdf" target="_blank" 
+        <a href="/MIDHAT_ULLAH_HASANI_FlowCV_Resume_2026-09-13.pdf" target="_blank" 
             aria-label="resume link"
             class="blueShadow relative overflow-hidden px-5 py-2 group rounded-full bg-white text-slate-950">
             <div class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"></div>
